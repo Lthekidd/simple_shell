8abb3ee3d25bf4977fcd6ba8596e55f5fdd5b612
Kevin Chiwakaya <kchiwakaya@gmail.com>
2023-07-25 13:32:52 +0100

    back to normal

--- path.c
+++ path.c
@@ -2,36 +2,37 @@
 #include "shell.h"
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * find_cmd - checks to see if cmd is an executable 
+ * @cmd: command
+ * @argv: vector array
+ * Return: 1 if executable present
+ */
 int find_cmd(char *cmd, char **argv)
 {
-  char *path = getenv("PATH");
-  int is_found = 0;
-  char *dir;
-  char *full_path;
+ 	char *path = getenv("PATH");
+	int is_found = 0;
+	char *dir;
+	char *full_path;
 
-  if (path == NULL)
-  {
-    perror(argv[0]);
-    return -1;
-  }
+	if (path == NULL)
+	{
+		perror(argv[0]);
+		return -1;
+	}
 
-  while (path != NULL)
-  {
-    dir = strtok(path, ":");
-    full_path = malloc(strlen(dir) + strlen(cmd) + 2);
-    strcpy(full_path, dir);
-    strcat(full_path, "/");
-    strcat(full_path, cmd);
-
-    if (access(full_path, X_OK) == 0) 
-    {
-      is_found = 1;
-      break;
-    }
-
-    free(full_path);
-    path = strtok(NULL, ":");
-  }
-  return is_found;
+	while (path != NULL)
+	{
+		dir = strtok(path, ":");
+ 		full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+		strcpy(full_path, dir);
+		strcat(full_path, "/");
+		strcat(full_path, cmd);
+	
+		if (access(full_path, X_OK) == 0)
+			is_found = 1;
+		free(full_path);
+        	path = strtok(NULL, ":");
+	}
+	return is_found;
 }
--- shell_execute.c
+++ shell_execute.c
@@ -11,50 +11,57 @@
  */
 void shell_execute(char **cmd, int argc, char **argv, char **envp)
 {
-    int status;
-    pid_t pid;
+	int status;
+	pid_t pid;
 	unsigned long int i;
 	size_t size;
 	int avail;
 	getfunction _functions[] = {{"cd" , shell_cd} , {"exit" , shell_exit} , {"env" , shell_env}};
 
 	size = sizeof(_functions)/sizeof(getfunction);
-    avail = find_cmd(cmd[0],argv);
-
-    if (avail)
-    {
-        perror(argv[0]);
-        return;
-    }
-
-    for (i = 0; i < size;i++)
-    {
-        if (_strcmp(_functions[i].name , cmd[0]) == 0)
-        {
-            _functions[i].getfunction(cmd, argc, envp);
-            return;
-        }
-    }
-    pid = fork(); /**Launch a child process*/
-    if (pid == 0)/**child process*/
-    {
-        execve(cmd[0],cmd,envp);/**run command*/
-        perror(argv[0]);
-        exit(1);
-    }
-    else if (pid > 0) /**parent process*/
-    {
-        do
-        {
-            /**wait for child process to exit*/
-		waitpid(pid,&status,WUNTRACED);
-        }
-        while(!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-    else
-    {
-	perror(argv[0]);
-    }
+	avail = find_cmd(cmd[0],argv);
+	
+	if (avail)
+  	{
+		perror(argv[0]);
+		return;
+	}
+	else if (avail == -1)
+	{
+		perror(argv[0]);
+		return;
+	}
+	else
+	{
+		for (i = 0; i < size;i++)
+		{
+			if (_strcmp(_functions[i].name , cmd[0]) == 0)
+			{
+				_functions[i].getfunction(cmd, argc, envp);
+				return;
+			}
+		}
+		pid = fork(); /**Launch a child process*/
+		if (pid == 0)/**child process*/
+		{
+			execve(cmd[0],cmd,envp);/**run command*/
+			perror(argv[0]);
+			exit(1);
+		}
+		else if (pid > 0) /**parent process*/
+		{
+			do
+			{
+				/**wait for child process to exit*/
+				waitpid(pid,&status,WUNTRACED);
+			}
+			while(!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+		else
+		{
+			perror(argv[0]);
+		}
+	}
 }
 
 
